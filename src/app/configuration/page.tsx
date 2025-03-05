c035828a455500430bc1f5a01a917a3d1560cb6c
prabhatsah <prabhatsah349@gmail.com>
2025-03-05 15:46:24 +0530

    added new dashboard without spinner

---
+++ src/app/configuration/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Scans } from "@/app/scans/components/Scans";
+import Layout from "@/components/Layout";
+import { useBreadcrumb } from "@/contexts/BreadcrumbContext";
+import { useEffect } from "react";
+
+export default function ScansPage() {
+  const { setItems } = useBreadcrumb();
+  useEffect(() => {
+    setItems([{ label: "Scans", href: "/scans" }]);
+  }, []);
+
+  return (
+    <Layout>
+      <Scans />
+    </Layout>
+  );
+}
