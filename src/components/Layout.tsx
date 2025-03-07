ffba9aafbcbfeb47d57559376629421ad6d9b287
Siddartha Kumar <siddartha.kumar@keross.com>
2025-03-07 11:15:30 +0530

    add background color to main content area in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden ">
         <Navbar />
-        <main className="flex-1 overflow-x-hidden overflow-y-auto ">
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-background">
           <div className="h-full p-6 bg-white dark:bg-gray-950">{children}</div>
         </main>
       </div>
