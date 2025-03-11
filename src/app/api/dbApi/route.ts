50d0ff08e18d0a39e0aa183b16a5444bd33e797e
sumalya <sumalya.mukherjee@keross>
2025-03-11 16:01:03 +0530

    commit

--- src/app/api/dbApi/route.ts
+++ src/app/api/dbApi/route.ts
@@ -3,6 +3,17 @@ import { NodeSSH } from "node-ssh";
 
 const ssh = new NodeSSH();
 
+
+function extractJson(input: string): any {
+  const match = input.match(/\[([\s\S]*)\]/);
+  
+  if (!match) {
+      throw new Error("No JSON found in the input string.");
+  }
+  const jsonString = match[0].replace(/\+\n/g, "").trim();
+      return JSON.parse(jsonString); 
+}
+
 export async function POST(req: Request) {
     try {
       let { query } = await req.json();
@@ -25,8 +36,12 @@ export async function POST(req: Request) {
   
       console.log("Query Result:", result);
       ssh.dispose();
-  
-    return NextResponse.json({ success: true, data: result });
+      let jsonData : any;
+      if(result.stdout.includes('json'))
+        jsonData = extractJson(result.stdout)
+
+      return NextResponse.json({ success: true, fullData: result , data : jsonData ? jsonData : null});
+
     } catch (error: any) {
       console.error("SSH Connection Failed:", error);
       return NextResponse.json({ success: false, error: error.message }, { status: 500 });
