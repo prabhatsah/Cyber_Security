74447a3494089fba498ead701da92ba98c5381b7
Siddartha Kumar <siddartha.kumar@keross.com>
2025-03-17 15:41:30 +0530

    Refactor Tabs component to improve styling and consistency

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -14,7 +14,7 @@ const Tabs: React.FC<TabsProps> = ({ tabs }) => {
 
   return (
     <div className="w-full">
-      <div className="flex border-gray-300 border-b">
+      <div className="flex border-b border-gray-200 dark:border-gray-800 inline-flex ">
         {tabs.map((tab, index) => (
           <button
             key={index}
