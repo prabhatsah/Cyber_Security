0fe3202b94b84987c5c83461add3b4d240f056fa
rickyroy11 <roysayan446@gmail.com>
2025-04-08 12:24:53 +0530

    Route change for Container Tools

--- src/app/configuration/container-tools/page.tsx
+++ src/app/configuration/container-tools/page.tsx
@@ -6,13 +6,13 @@ export default function ContainerServicesConfig() {
       <RenderAppBreadcrumb
         breadcrumb={{
           level: 1,
-          title: "Container Services",
-          href: "/configuration/container-services"
+          title: "Container Tools",
+          href: "/configuration/container-tools"
         }}
       />
       <div className="px-6 py-3">
         <h2 className="text-2xl font-semibold text-primary">
-          Container Services
+          Container Tools
         </h2>
       </div>
     </>
