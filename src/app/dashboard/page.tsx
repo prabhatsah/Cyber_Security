38c5ce298e71dda42a4329aff40403d9ebd9333e
sumalya <sumalya.mukherjee@keross>
2025-05-05 13:46:20 +0530

    ws changes added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,29 @@
 import Dashboard from "@/app/dashboard/components/Dashboard";
 import { RenderAppBreadcrumb } from "@/components/app-breadcrumb";
 import setBreadcrumb from "@/lib/setBreadcrumb";
+import { getSocket } from '@/utils/webSocketComponent';
+import { useEffect } from "react";
 
 export default function Home() {
-  // setBreadcrumb([{ label: "Dashboard", href: "/dashboard" }]);
+  // useEffect(() => {
+  //   const socket = getSocket();
+  //   console.log('inside first useEffect')
+
+  //   socket.on('connect', () => {
+  //     console.log('Socket connected:', socket.id);
+  //   });
+
+  //   const onCustomMessage = (data: any) => {
+  //     console.log('📨 Received from socket:', data);
+  //   };
+
+  //   socket.on('custom_event', onCustomMessage);
+
+  //   return () => {
+  //     socket.off('custom_event', onCustomMessage);
+  //     socket.off('connect');
+  //   };
+  // }, []);
 
   return <>
     <RenderAppBreadcrumb
