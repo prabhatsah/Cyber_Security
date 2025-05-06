1d2689d128e4cf2fce237c5acb1049345dd7a3af
Siddartha Kumar <siddartha.kumar@keross.com>
2025-05-06 10:38:28 +0530

    changes

--- src/app/scans/WebApi/page.tsx
+++ src/app/scans/WebApi/page.tsx
@@ -3,7 +3,7 @@ import Scan from "./components/Scan";
 export default function WebApi() {
 
   return (
-    <div className="p-4">
+    <div className="">
       <p className="font-bold text-widget-title text-pageheader">Web & API Security</p>
       <Scan />
     </div >
