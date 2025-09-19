a5664a9d06522078c6d7079a53b8db09ea3c06b5
rickyroy11 <roysayan446@gmail.com>
2025-09-19 14:55:45 +0530

    Network Pentest live data and Enumeration Dummy Data

--- src/app/pen-test/network-pen-test/page.tsx
+++ src/app/pen-test/network-pen-test/page.tsx
@@ -1,81 +1,42 @@
 import { RenderAppBreadcrumb } from "@/components/app-breadcrumb";
 import NetworkPentestWidget from "./components/NetworkPentestWidget";
-import { getLoggedInUserProfile } from "@/ikon/utils/api/loginService";
-import { fetchData } from "@/utils/api";
 import NoSavedPentestTemplate from "./components/NoSavedPentestTemplate";
-import { NetworkPenTestWithoutScanModified } from "../../NetworkPentestType";
-// import AddPentestBtnWithFormModal from "./components/AddPentestBtnWithFormModal";
+import { NetworkPentestFullInstanceData } from "../../NetworkPentestType";
+import { getCurrentUserId } from "@/ikon/utils/actions/auth";
+import { getCurrentSoftwareId } from "@/ikon/utils/actions/software";
+import { getUserDashboardPlatformUtilData } from "@/ikon/utils/actions/users";
+import { getMyInstancesV2 } from "@/ikon/utils/api/processRuntimeService";
+import { createUserMap } from "@/app/utils/UserDetailsUtils";
 
 
-async function fetchLoggedInUserPentestData() {
-    const userId = (await getLoggedInUserProfile()).USER_ID;
+const fetchPresentUserConfigDetails = async () => {
+    const presentUserId = await getCurrentUserId();
+    const softwareId = await getCurrentSoftwareId();
+    const pentestAdminGroupDetails = await getUserDashboardPlatformUtilData({ softwareId, isGroupNameWiseUserDetailsMap: true, groupNames: ["Pentest Admin"] });
+    const pentestAdminUsers = Object.keys(pentestAdminGroupDetails["Pentest Admin"].users);
 
-    console.log("user id ---------")
-    console.log(userId);
+    console.log("Pentest Admin Users: ", pentestAdminUsers);
 
+    const configInstances = await getMyInstancesV2<NetworkPentestFullInstanceData>({
+        processName: "Network Pentest",
+        predefinedFilters: { taskName: "Pentest Details View" },
+        processVariableFilters: pentestAdminUsers.includes(presentUserId) ? null : { created_by_id: presentUserId },
+        projections: ["Data"],
+    });
 
-    const fetchedData = await fetchData('pentest_data', 'last_scan_on', [{ table: "user_membership", column: 'type', value: 'network' }], null,
-        "pentest_data.pentestid, pentest_data.data->'basicDetails' as basicdetails, pentest_data.last_scan_on");
+    let configDataArray: NetworkPentestFullInstanceData[] = [];
+    if (configInstances.length) {
+        configDataArray = configInstances.map(eachInstance => eachInstance.data);
+    }
 
-    return fetchedData;
+    return configDataArray;
 }
 
-const loggedInUserPentestData: NetworkPenTestWithoutScanModified[] = [
-    {
-        pentestId: "KER-NET-2507001",
-        pentestType: "network",
-        networkDetails: {
-            pentestId: "KER-NET-2507001",
-            clientName: "Carta Worldwide",
-            pentestName: "Canadian Internal Network Pentest - Q1 2025",
-            scopeType: "CIDR Network Range",
-            aggressiveness: "Stealthy",
-            progress: 10,
-            startDate: "2025-07-25 10:12",
-            createdOn: "2025-07-25 10:12",
-            createdBy: {
-                userId: 'be7a0ece-f3d8-4c5b-84dc-52c32c4adff4',
-                userName: "Sayan Roy"
-            }
-        },
-        lastUpdated: "2025-07-25 10:12",
-        userId: 'be7a0ece-f3d8-4c5b-84dc-52c32c4adff4',
-    },
-    {
-        pentestId: "KER-NET-2507002",
-        pentestType: "network",
-        networkDetails: {
-            pentestId: "KER-NET-2507002",
-            clientName: "TechCorp Solutions",
-            pentestName: "External Network Assessment - Q2 2025",
-            scopeType: "CIDR Network Range",
-            aggressiveness: "Normal",
-            progress: 10,
-            startDate: "2025-07-25 11:12",
-            createdOn: "2025-07-25 11:12",
-            createdBy: {
-                userId: 'be7a0ece-f3d8-4c5b-84dc-52c32c4adff4',
-                userName: "Sayan Roy"
-            }
-        },
-        lastUpdated: "2025-07-25 11:12",
-        userId: 'be7a0ece-f3d8-4c5b-84dc-52c32c4adff4',
-    }
-]
-
 export default async function NetworkPenetrationTesting() {
+    const presentUserConfigDetails: NetworkPentestFullInstanceData[] = await fetchPresentUserConfigDetails();
+    console.log("Fetched Config Details: ", presentUserConfigDetails);
 
-    // const loggedInUserPentestData: NetworkPenTestWithoutScanDefault[] = await fetchLoggedInUserPentestData() ? (await fetchLoggedInUserPentestData()).data : [];
-    // const loggedInUserPentestDataFormatted: NetworkPenTestWithoutScanModified[] = loggedInUserPentestData.map((eachPenTestData: NetworkPenTestWithoutScanDefault) => {
-    //     return {
-    //         userId: eachPenTestData.userid,
-    //         pentestId: eachPenTestData.pentestid,
-    //         pentestType: eachPenTestData.type,
-    //         basicDetails: { ...eachPenTestData.basicdetails },
-    //         lastUpdated: eachPenTestData.lastscanon,
-    //     }
-    // });
-    // console.log("Pentest Data With New Func: ", loggedInUserPentestDataFormatted);
+    const userIdNameMap: { value: string; label: string }[] = await createUserMap();
 
     return (
         <>
@@ -98,15 +59,16 @@ export default async function NetworkPenetrationTesting() {
                 <div className='flex items-center justify-between mb-4'>
                     <div className="flex items-center space-x-2">
                         <h3 className="text-widgetHeader text-primary">
-                            {loggedInUserPentestData.length > 1 ? `Configured Penetration Tests` : `Configured Penetration Test`}
+                            {presentUserConfigDetails.length > 1 ? `Configured Penetration Tests` : `Configured Penetration Test`}
                         </h3>
                         <span className="inline-flex size-7 items-center justify-center rounded-full bg-tremor-background-subtle text-tremor-label font-medium text-tremor-content-strong dark:bg-dark-tremor-background-subtle dark:text-dark-tremor-content-strong">
-                            {loggedInUserPentestData.length}
+                            {presentUserConfigDetails.length}
                         </span>
                     </div>
                     {/* <AddPentestBtnWithFormModal /> */}
                 </div>
-                {loggedInUserPentestData.length === 0 ? <NoSavedPentestTemplate /> : <NetworkPentestWidget allPentestWidgetData={loggedInUserPentestData} />}
+                {presentUserConfigDetails.length === 0 ? <NoSavedPentestTemplate /> :
+                    <NetworkPentestWidget allPentestWidgetData={presentUserConfigDetails} userIdNameMap={userIdNameMap} />}
             </div>
         </>
     );
